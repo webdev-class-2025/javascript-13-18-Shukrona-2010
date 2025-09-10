f77d2f54b1601b258bdfd0c1799dc9cfb410e446
Shukrona Asanbekovna <shukronakarimdodova@gmail.com>
2025-09-10 17:44:16 +0500

    My homework

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let userName = "Shukrona"
+let age = 15;
+let city = "Khorog";
 
+console.log(`My name is ${userName}, I am ${age} year, i am from ${city} city.`);
+
+if (age >=18) {
+    console.log("Savershenoletne");
+} else{
+    console.log("Nesovershenoletne");
+}
+
+function greet(name){
+    console.log(`Welcome, ${name}`);
+}
+
+greet("Shukrona");
+
+function multiply(a, b){
+    return a*b;
+}
+
+console.log(multiply(10, 20));
+
+for(let i=1;i<=10;i++){
+    if(i%2===0){
+        console.log(`${i} - чётное`);
+    }else{
+        console.log(`${i} - nechetnoye`);
+    }
+}
